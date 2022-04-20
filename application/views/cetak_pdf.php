82842910ea7a38815e53b4f9073686d0ab129ee9
RichoMaylano <maylanoricho@gmail.com>
2022-04-20 08:00:42 +0700

    Fitur Home, Obat, Rawat, dan Rawat Obat by Richo

--- application/views/cetak_pdf.php
+++ application/views/cetak_pdf.php
@@ -31,8 +31,6 @@
       
 
         $pdf->SetFont('Arial','',10);
-        // $id = $_GET['id'];
-        // $data['rawat'] = $this->Rawat_model->get_single_row_rawat($id);
         foreach ($data as $row){
             $pdf->Cell(60,15,$row->tglrawat,1,0,'C');
             $pdf->Cell(60,15,"Rp ".number_format($row->totaltindakan, 0, ".", "."),1,0,'C');
@@ -43,28 +41,15 @@
         $pdf->Cell(0,12,'* Biaya Pengeluaran',0,1,'L');
         $pdf->Cell(10,1,'',0,1);
 
-        // $pdf->SetFont('Arial','B',10);
-
-        // $pdf->Cell(60,15,'TOTAL HARGA',1,0,'C');
-        // $pdf->Cell(60,15,'UANG MUKA',1,0,'C');
-        // $pdf->Cell(60,15,'KEKURANGAN',1,1,'C');
-
-        // $pdf->SetFont('Arial','',10);
-        //    $id = $_GET['id'];
-        // $data['rawat'] = $this->Rawat_model->get_single_row_rawat($id);
-        // foreach ($data as $row){
-        //     $pdf->Cell(60,15,"Rp ".number_format($row->totalharga, 0, ".", "."),1,0,'C');
-        //     $pdf->Cell(60,15,"Rp ".number_format($row->uangmuka, 0, ".", "."),1,0,'C');
-        //     $pdf->Cell(60,15,"Rp ".number_format($row->kurang, 0, ".", "."),1,1,'C');
- $pdf->SetFont('Arial','',10);
-             foreach ($data as $row){
-       $pdf->Cell(0,7,"Total Harga (Rp)       :  ".number_format($row->totalharga, 0, ".", "."),0,1,'L');
-            $pdf->Cell(0,7,"Uang Muka (Rp)       :  ".number_format($row->uangmuka, 0, ".", "."),0,1,'L');
-            $pdf->Cell(0,7,"Kekurangan (Rp)      :  ".number_format($row->kurang, 0, ".", "."),0,1,'L');
+        $pdf->SetFont('Arial','',10);
+        foreach ($data as $row){
+        $pdf->Cell(0,7,"Total Harga (Rp)       :  ".number_format($row->totalharga, 0, ".", "."),0,1,'L');
+        $pdf->Cell(0,7,"Uang Muka (Rp)       :  ".number_format($row->uangmuka, 0, ".", "."),0,1,'L');
+        $pdf->Cell(0,7,"Kekurangan (Rp)      :  ".number_format($row->kurang, 0, ".", "."),0,1,'L');
         $pdf->Cell(10,7,'',0,1);
-     
-        }
-  $pdf->Cell(10,50,'',0,1);
+          }
+
+        $pdf->Cell(10,50,'',0,1);
         $pdf->SetFont('Arial','B',12);
         $pdf->Cell(0,12,'***TERIMA KASIH***',0,1,'C');
         $pdf->Cell(10,1,'',0,1);
