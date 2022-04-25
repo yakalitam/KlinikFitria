a27dda4a855b839e97ffc4c5b27d78aec716c0d3
MIlyas777 <maulanailyas95gmail.com>
2022-04-25 13:35:50 +0700

    update swar

--- application/views/pasien.php
+++ application/views/pasien.php
@@ -39,7 +39,7 @@
                 <table id="Tpasien" class="table table-hover table-bordered rounded-5 text-center">
                     <thead>
                         <tr>
-                            <th scope="col" style="width: 20px;">No</th>
+                            <!-- <th scope="col" style="width: 20px;">No</th> -->
                             <th scope="col" style="width: 100px;">ID Pasien</th>
                             <th scope="col" style="width: 500px;">Nama Pasien</th>
                             <th scope="col" style="width: 200px;">Tanggal Lahir</th>
@@ -49,7 +49,7 @@
                         </tr>
                     </thead>
 
-                    <tbody>
+                    <!-- <tbody>
                         <?php
                         $no = 1;
                         foreach ($data as $d) { ?>
@@ -62,21 +62,73 @@
                                 <th><?= $d['notelp'] ?></th>
                                 <th>
                                     <a class="btn btn-sm btn-warning" href="Pasien/edit/<?php echo $d['idpasien'] ?>"><i class="icon-pencil"></i> EDIT</a>
-                                    <a class="btn btn-sm btn-danger" href="Pasien/delete/<?php echo $d['idpasien']; ?>" onclick="return konfirmasi('Data ini akan dihapus, apakah anda yakin?')">DELETE</a>
+                                    <button class="btn btn-sm btn-danger"  onclick="return fungsiDelete()">DELETE</button>
                                 </th>
                             </tr>
                         <?php } ?>
-                    </tbody>
+                    </tbody> -->
                 </table>
             </div>
 
         </div>
     </div>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<?php if ($this->session->flashdata('pesan') !=''){ ?>
+    <script> Swal.fire({
+        icon  : 'success',
+        title : 'SUKSES',
+        text  : 'lancar gaes',
+        confirmButtonText: 'sap lur'
+    })</script>
+<?php } elseif ($this->session->flashdata('gagal') !=''){ ?>
+    Swal.fire({
+        icon  : 'error',
+        title : 'FAIL',
+        text  : 'Gagal : what's problem?',
+        confirmButton : 'santuy'
+    })
+<?php } ?>
+
 
     <script>
+        function fungsiDelete(){
+            Swal.fire({
+                title: 'Do you want to save the changes?',
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: 'Hapus',
+                denyButtonText: `Jangan Hapus `,
+                }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    Swal.fire('Hapus!', '', 'success');
+                    location.href = "Pasien/delete/<?php echo $d['idpasien']; ?>"
+
+                } else if (result.isDenied) {
+                    Swal.fire('Changes are not saved', '', 'info')
+                }
+        })
+        }
+        function fungsiEdit(){
+            location.href = "Pasien/edit/<?php echo $d['idpasien'] ?>";
+        }
+</script>
+<script>
         // script untuk menampilkan table dengan library datatables
         $(document).ready(function() {
-            $('#Tpasien').DataTable();
-            responsive: true
+            $('#Tpasien').DataTable({
+            responsive: true,
+            processing: true,
+            ajax:"<?= base_url('Pasien/data')?>",
+            columnDefs: [{
+                defaultContent: klikTombol(),
+                targets: [5]
+            }]
+            })
         });
+
+        function klikTombol(){
+            return `<button class="btn btn-sm btn-warning" onclick="return fungsiEdit()"><i class="icon-pencil"></i> EDIT</button>
+                                    <button class="btn btn-sm btn-danger"  onclick="return fungsiDelete()">DELETE</button>`;
+        }
     </script>
